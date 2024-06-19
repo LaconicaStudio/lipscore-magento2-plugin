ae65376bd1445f083e0017225a33ed6720491e34
Oleksandr Pliatsyk <oleksandr.pl@laconica.com>
2024-06-19 13:12:55 +0000

    Product testimonials configuration

---
+++ Block/Product/Testimonials.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Lipscore\RatingsReviews\Block\Product;
+
+use Lipscore\RatingsReviews\Block\AbstractWidget;
+
+class Testimonials extends AbstractWidget
+{
+    protected $_template = 'testimonials.phtml';
+}
